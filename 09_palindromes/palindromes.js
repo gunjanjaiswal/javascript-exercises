a3ddffcea8c6a59c1f39c10ae0ffb9d704ca6adf
gunjanjaiswal <166167215+gunjanjaiswal@users.noreply.github.com>
2025-03-22 22:18:09 +0530

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
-
+const palindromes = function (string){
+    let start=0;
+    let end=string.length-1;
+    while(start<end){
+        while(!checkAlphanum(string[start])){
+            start++;
+        }
+        while(!checkAlphanum(string[end])){
+            end--;
+        }
+        if(start<end && string[start].toLowerCase()!=string[end].toLowerCase()){
+            console.log("start",string[start]," end ",string[end])
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
 };
+
+function checkAlphanum(character) {
+    n=character.charCodeAt(0);
+    return (n >= 65 && n < 91) || (n >= 97 && n < 123) || (n>=48 && n<=57)
+}
 
 // Do not edit below this line
 module.exports = palindromes;
